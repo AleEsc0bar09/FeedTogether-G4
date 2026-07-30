c0d2cf4fe18df327e370bdabdc9a615dfcc72852
AngelByT <angel.valladares2027@frech.superate.org>
2026-07-30 14:36:50 -0600

    Public files creation

--- FeedTogether/scripts/scripts.js
+++ FeedTogether/scripts/scripts.js
@@ -10,7 +10,7 @@ function loadSection(sectionName) {
     return;
   }
 
-  fetch(`sections/${sectionName}.html`)
+  fetch(`public/${sectionName}.html`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`Failed to load section: ${response.statusText}`);
